00adb74733e713823d7f940362aa901a2d3bb142
Chase Thompson <chaseth@me.com>
2019-01-22 08:44:21 -0600

    Added Game class, and created phrases for the game
    Created the Game class and added all the phrases that will be chosen randomly later on.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,37 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+/** 
+ * OOP Game App
+ * Game.js 
+ */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+    /**
+     * Creates the phrases used in the game
+     * @return {array} An array of all phrases used for game, to be chosen randomly later
+     */
+    createPhrases() {
+        let phrases =[{
+            phrase: 'sometimes i have believed as many as six impossible things before breakfast',
+            },
+            {
+            phrase: 'unless someone like you cares a whole awful lot, nothing is going to get better it is not.',
+            },
+            {
+            phrase: 'it is our choices that show what we truly are far more than our abilities',
+            },
+            {
+            phrase: 'if it is on the internet it must be true'
+            },
+            {
+            phrase: 'the game is afoot'
+            }];
+            return phrases;
+        }
+    
+    
+    }
